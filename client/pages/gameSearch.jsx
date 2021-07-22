4ccb37c9005b66142de9a7c441a29e640980e550
Victor Kalinkin <vakalinkin@hotmail.com>
2021-07-22 12:21:34 -0700

    added a search text input and button that accepts user input

--- client/pages/gameSearch.jsx
+++ client/pages/gameSearch.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 // import { render } from 'react-dom';
 
 export default class GameSearch extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = { searchTerm: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  handleSubmit(event) {
+    // eslint-disable-next-line no-console
+    console.log('Term entered:', this.state.searchTerm);
+    event.preventDefault();
+  }
 
   render() {
     return (
@@ -18,6 +31,10 @@ export default class GameSearch extends React.Component {
         </div>
         <h1>Temp: On Game Search Page</h1>
         <a href="#">go back to home</a>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.searchTerm} onChange={this.handleChange}></input>
+          <button>SEARCH</button>
+        </form>
       </div>
     );
   }
